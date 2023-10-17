07324e56f82462a0242f665fcd2aad4fd3d8da10
Ghazanfar Ali <gak_92@hotmail.com>
2023-10-17 13:49:02 +0500

    Day3 - using variables

---
+++ day_03/05_variables.py
@@ -0,0 +1,30 @@
+# Variables
+# 1- Should contain letters, numbers and underscore
+# 2- Do not start with numbers
+# 3- Spaces not allowed
+# 4- Do not use keywords
+# 5- Use descriptive
+# 6- Case sensitive
+
+x = 5
+print(x)
+
+y = "Learning python"
+print(y)
+
+x = x + 5
+print(x)
+
+print(type(x))
+print(type(y))
+
+fruit_basket = 8
+print(fruit_basket)
+print(type(fruit_basket))
+
+fruit_basket = "Mangoes"
+print(fruit_basket)
+print(type(fruit_basket))
+
+del fruit_basket
+print(fruit_basket)   # NameError: name 'fruit_basket' is not defined
