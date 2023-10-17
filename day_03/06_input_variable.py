1dc222b815dda0537347a34767a05ee1e41dd0d1
Ghazanfar Ali <gak_92@hotmail.com>
2023-10-17 13:57:36 +0500

    Day3 - input variables

---
+++ day_03/06_input_variable.py
@@ -0,0 +1,11 @@
+fruit = input("What is your favorite food? ")
+print(fruit)
+
+name = input("What is your name? ")
+greeting = "Hello"
+print(greeting, name)
+
+print("Hi!", name)
+
+age = input("enter your age: ")
+print("Hello", name, "Your age is", age)
