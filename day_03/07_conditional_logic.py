cfd24e5a67fcf256888ece075d49498218d5b947
Ghazanfar Ali <gak_92@hotmail.com>
2023-10-17 14:08:44 +0500

    Day3 - conditional logic

---
+++ day_03/07_conditional_logic.py
@@ -0,0 +1,27 @@
+# Logical operators
+# "True or False", "Yes or No", "0 or 1"
+# equal to                  ==
+# not equal to              !=
+# greater than               >
+# less than                 <
+# greater than or equal      >=
+# less than or equal         <=
+
+# is 4 equal to 4?
+print(4==4)
+print(4!=4)
+print(3<4)
+print(3>4)
+
+print(3>=4)
+print(3<=4)
+
+school_age = 5
+your_age = input("What is your age? ")
+print(type(your_age))
+print(your_age == school_age)
+
+your_age = int(your_age)
+print(type(your_age))
+
+print(your_age == school_age)
