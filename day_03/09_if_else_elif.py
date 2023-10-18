0a2d4f82d85d9de19e336a98c4021a7f4a3536c4
Ghazanfar Ali <gak_92@hotmail.com>
2023-10-18 11:43:20 +0500

    Day3- if else and elif

---
+++ day_03/09_if_else_elif.py
@@ -0,0 +1,13 @@
+# if, else, and elif
+
+your_age = 1
+school_age = 5
+
+if your_age == school_age:
+  print("You should go to school")
+elif your_age > school_age:
+  print("You should go to college")
+elif your_age <=2:
+  print("you are still young")
+else:
+  print("You cannot go to school")
