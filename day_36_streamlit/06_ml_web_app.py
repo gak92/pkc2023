e9402b0b7e16b80872de348fb2d70a3213d269b3
Ghazanfar Ali <gak_92@hotmail.com>
2024-03-04 01:09:29 +0500

    comment seaborn library

--- day_36_streamlit/06_ml_web_app.py
+++ day_36_streamlit/06_ml_web_app.py
@@ -1,7 +1,7 @@
 # import libraries
 import numpy as np
 import pandas as pd
-import seaborn as sns
+# import seaborn as sns
 import matplotlib.pyplot as plt
 import streamlit as st
 from sklearn import datasets
